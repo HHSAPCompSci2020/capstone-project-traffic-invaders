6f56102a82d983efdbb29679fb0bd52e002c4a20
Karan Dalal <karansdalal@gmail.com>
2021-05-07 14:09:01 -0700

    Version :-1:

--- src/Game.java
+++ src/Game.java
@@ -6,13 +6,16 @@ import screens.Screen;
  * The Game class represents the actual game panel where
  * the game is played on
  * @author Karan Dalal
- * @verion 1.0
+ * @version 1.0
  *
  */
 public class Game extends PApplet{
 	
 	private Screen screen;
 
+	/**
+	 * The setup method sets up the game panel for play with the MainMenu screen.
+	 */
 	public void setup() {
 		screen = new MainMenu();
 		surface.setSize(400,300);
@@ -20,6 +23,9 @@ public class Game extends PApplet{
 
 	}
 	
+	/**
+	 * The draw method constantly uns and updates the screen.
+	 */
 	public void draw() {
 		background(0,0,0);
 		screen.draw(this);
