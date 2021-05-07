1b965502a609042c1a33e6fda1b089f6b20b833f
Karan Dalal <karansdalal@gmail.com>
2021-05-07 14:08:39 -0700

    Javadoc :)

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,13 @@ import processing.core.PApplet;
 import screens.MainMenu;
 import screens.Screen;
 
+/**
+ * The Game class represents the actual game panel where
+ * the game is played on
+ * @author Karan Dalal
+ * @verion 1.0
+ *
+ */
 public class Game extends PApplet{
 	
 	private Screen screen;
