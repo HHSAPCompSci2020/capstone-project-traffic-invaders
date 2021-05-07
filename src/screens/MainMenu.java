accdbf36d21500833b8d8e5e600773b3c0899564
vikram087 <70039401+vpenumarti@users.noreply.github.com>
2021-05-07 14:19:13 -0700

    Main Menun javadocs

--- src/screens/MainMenu.java
+++ src/screens/MainMenu.java
@@ -2,19 +2,35 @@ package screens;
 
 import processing.core.PApplet;
 
+/**
+ * Is the opening screen which the player will see when they first launch the game, die, or want to quit
+ * @author Eric Decasper
+ * @version 1.0
+ */
 public class MainMenu extends Screen{
+	
+	/**
+	 * Will initialize variables here later on
+	 */
 	public MainMenu() {
 		
 	}
 
-	@Override
+
+	/**
+	 * Draws the main menu on the drawing surface
+	 * @param PApplet g - A PApplet object which sets the main menu
+	 */
 	public void draw(PApplet g) {
 		g.background(0, 204, 204);// Background is set to cyan
 		g.stroke(0, 0, 0); //sets the stroke to black
 		g.text("MAINMENU", g.width / 2 - g.textWidth("MAINMENU") / 2, g.height / 2); 
 	}
 
-	@Override
+	
+	/**
+	 * Updates the screen
+	 */
 	public void update() {
 		// TODO Auto-generated method stub
 		
